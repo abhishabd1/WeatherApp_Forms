488debd5f21a5bfc849a7ada82a996b7773ebd3c
Abhishek Kumar <abhishabd1@outlook.com>
2017-10-31 16:24:46 +0530

    initail commit

---
+++ WeatherAppForms/WeatherAppForms/Core.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppForms
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipcode)
+        { 
+        //Forming a url
+        string key = "e707aad5de8d6b92c2ceab1bbf47ec65";
+
+        //samples.openweathermap.org/data/2.5/forecast?id=845438&appid=b1b15e88fa797225412429c1c50c122a1
+    //    api.openweathermap.org/data/2.5/weather?zip=560061,us&appid=e707aad5de8d6b92c2ceab1bbf47ec65
+
+
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + key +"";
+           
+            dynamic results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+
+            if(results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        }
+    
+}
---
+++ WeatherAppForms/WeatherAppForms/WeatherPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using WeatherAppForms;
+
+namespace WeatherAppForms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+
+            this.Title = "Weather App";
+            getWeatherBtn.Clicked += GetWeatherBtn_Clicked;
+
+
+
+
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if(zipCodeEntry.Text != null)
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                this.BindingContext = weather;
+                getWeatherBtn.Text = "Search Again";
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
